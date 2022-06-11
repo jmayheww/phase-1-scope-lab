4a3f219fad52e78b00bcaa2b1afd45db49e070bf
Joshua <joshsmayhew@gmail.com>
2022-06-11 15:14:11 +0700

    commit tree fc6adc3089bfcefed624f1b2beb2d2fa64df10d4

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
 // Write your solution in this file!
+
+
+function setGlobalScope () {
+    window.customerName = 'bob';
+    return upperCaseCustomerName = () => {
+        window.customerName = customerName.toUpperCase();
+    }
+}
+
+
+setGlobalScope();
+
+
+
+    function setBestCustomer() {
+        window.bestCustomer = 'not bob';
+
+    }
+
+
+
+    function overwriteBestCustomer() {
+        window.bestCustomer = 'maybe bob';
+    }
+
+    const leastFavoriteCharacter = 'dingo';
+    function changeLeastFavoriteCustomer() {
+        leastFavoriteCharacter = 'hugo';
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const setBestCustomer = () => {
+//   window.bestCustomer = "not bob";
+// };
+
+// setBestCustomer();
+
+// const overwriteBestCustomer = () => {
+//   window.bestCustomer = "maybe bob";
+// };
+
+// overwriteBestCustomer();
+
+// const changeLeastFavoriteCustomer = () =>  {const changeLeastFavoriteCustomer = 'Josh'};
+
+// changeLeastFavoriteCustomer();
